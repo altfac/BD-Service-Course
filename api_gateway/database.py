750297357df9a5e23124b0b58767ca9c9d05c1c6
Razorlanca1 <kartem423@gmail.com>
2024-12-19 21:54:15 +0300

    education page added

---
+++ api_gateway/database.py
@@ -0,0 +1,111 @@
+import mysql.connector, json
+
+class Datebase:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def get_all_education_materials(self):
+        self.connection.reconnect()
+        cursor = self.connection.cursor()
+        courses, difficulty_levels = {}, {}
+        data = {}
+
+        cursor.execute("Select * from difficulty_level")
+        ret = cursor.fetchall()
+        for i in ret:
+            difficulty_levels[i[0]] = i[1]
+
+        cursor.execute("Select * from course")
+        ret = cursor.fetchall()
+        for i in ret:
+            data[i[1]] = {}
+            courses[i[0]] = i[1]
+            for level in difficulty_levels.keys():
+                data[i[1]][difficulty_levels[level]] = []
+
+        cursor.execute("Select * from chapter")
+        ret = cursor.fetchall()
+        for i in ret:
+            # task = list(i[1:])
+            course = courses[i[3]]
+            level = difficulty_levels[i[4]]
+            data[course][level].append([i[0], i[1]])
+            # tasks[i[0]][2] = courses[i[3]]
+            # tasks[i[0]][3] = difficulty_levels[i[4]]
+            # for ind in range(len(i[2])):
+            #     self.cursor.execute(f"select * from education_material where Educational_Material={i[2][ind]}")
+            #     tasks[i[0]][1][ind] = self.cursor.fetchall()[0][1:]
+
+        return data
+
+    def get_education_material(self, id):
+        try:
+            self.connection.reconnect()
+            cursor = self.connection.cursor()
+            cursor.execute(f"Select * from chapter where Chapter_ID={id}")
+            data = list(cursor.fetchone())[:3]
+            # self.cursor.execute(f"Select Degree from difficulty_level where Difficulty_Level_ID={data[4]}")
+            # data[4] = self.cursor.fetchone()[0]
+            # self.cursor.execute(f"Select Title from course where Course_ID={data[3]}")
+            # data[4] = self.cursor.fetchone()[0]
+            data[2] = json.loads(data[2])
+            for i in range(len(data[2])):
+                cursor.execute(f"select * from education_material where Educational_Material={data[2][i]}")
+                data[2][i] = cursor.fetchone()
+
+            return data
+        except Exception:
+            return None
+
+    def delete_education_material(self, task_id, material_id):
+        try:
+            self.connection.reconnect()
+            cursor = self.connection.cursor()
+            cursor.execute(f"Select * from chapter where Chapter_ID={task_id}")
+            data = json.loads(cursor.fetchone()[2])
+            data.remove(material_id)
+            data = json.dumps(data)
+            cursor.execute(f"UPDATE chapter set Content='{data}' where Chapter_ID={task_id}")
+            self.connection.commit()
+        except Exception:
+            pass
+
+    def delete_task(self, id):
+        try:
+            self.connection.reconnect()
+            cursor = self.connection.cursor()
+            cursor.execute(f"DELETE FROM chapter WHERE Chapter_ID={id}")
+            self.connection.commit()
+        except Exception:
+            pass
+
+    def add_material(self, id, type, name):
+        try:
+            self.connection.reconnect()
+            cursor = self.connection.cursor()
+            cursor.execute(f"INSERT INTO education_material (Type, Content) VALUES ('{type}', '{name}')")
+            cursor.execute(f"SELECT Content FROM chapter WHERE Chapter_ID = {id}")
+            data = json.loads(cursor.fetchone()[0])
+            cursor.execute(f"SELECT Educational_Material From education_material WHERE (Type, Content) = ('{type}', '{name}')")
+            data.append(cursor.fetchone()[0])
+            data = json.dumps(data)
+            cursor.fetchall()
+            cursor.execute(f"UPDATE chapter set Content='{data}' where Chapter_ID={id}")
+            self.connection.commit()
+        except Exception:
+            pass
+
+    def add_task(self, course, level, name):
+        try:
+            self.connection.reconnect()
+            cursor = self.connection.cursor()
+            cursor.execute(f"SELECT Course_ID FROM course WHERE Title = '{course}'")
+            course_id = cursor.fetchall()[0][0]
+            cursor.execute(f"SELECT Difficulty_Level_ID FROM difficulty_level WHERE Degree = '{level}'")
+            level_id = cursor.fetchall()[0][0]
+            cursor.execute(f"INSERT INTO chapter (Title, Content, CourseCourse_ID, "
+                           f"Difficulty_LevelDifficulty_Level_ID, admin_Admin_ID) "
+                           f"VALUES ('{name}', '[]', {course_id}, {level_id}, 1)")
+            self.connection.commit()
+        except Exception:
+            pass
---
+++ api_gateway/main.py
@@ -0,0 +1,159 @@
+import datetime
+from fastapi import FastAPI, Request, Response, Form, Cookie, UploadFile
+from fastapi.responses import JSONResponse
+import requests
+from starlette.responses import RedirectResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+from database import *
+
+SECRET_KEY = "09d25e094faa9154613450832486f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+
+
+app = FastAPI(title="Curse", description="", version="1.0")
+templates = Jinja2Templates(directory="../templates")
+connection = mysql.connector.connect(host="localhost", user="root", password="root", database="curseusers")
+my_database = Datebase(connection)
+my_database.get_all_education_materials()
+
+
+def check_auth():
+    return True
+
+
+def auth():
+    pass
+
+
+@app.get("/")
+async def auth(request: Request):
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    params = {"request": request}
+    return templates.TemplateResponse("html/main.html", params, media_type="text/html")
+
+
+@app.get("/theory_tasks_edit")
+async def theory_tasks_edit(request: Request):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    params = {"request": request, "task": my_database.get_all_education_materials(), "current": "Theory tasks edit"}
+    return templates.TemplateResponse("html/theory_tasks.html", params, media_type="text/html")
+
+
+@app.get("/practical_tasks_edit")
+async def practical_tasks_edit(request: Request):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    params = {"request": request, "task": {}, "current": "Practical tasks edit"}
+    return templates.TemplateResponse("html/practical_tasks.html", params, media_type="text/html")
+
+
+@app.get("/users_edit")
+async def users_edit(request: Request):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    params = {"request": request, "task": {}, "current": "User edit"}
+    return templates.TemplateResponse("html/users.html", params, media_type="text/html")
+
+
+@app.get("/task/{id}")
+async def get_task(request: Request, id: int):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    task = my_database.get_education_material(id)
+    if not task:
+        return RedirectResponse("/")
+
+    params = {"request": request, "task": task, "current": "Task"}
+    return templates.TemplateResponse("html/task.html", params, media_type="text/html")
+
+
+@app.get("/delete_material/{task_id}/{material_id}")
+async def delete_education_material(request: Request, task_id: int, material_id: int):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    my_database.delete_education_material(task_id, material_id)
+
+    return RedirectResponse(f"/task/{task_id}")
+
+
+@app.get("/delete_task/{id}")
+async def delete_task(request: Request, id: int):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    my_database.delete_task(id)
+
+    return RedirectResponse(f"/theory_tasks_edit")
+
+
+@app.post("/add_material/{id}")
+async def delete_task(file: UploadFile, id: int, type: str = Form(None)):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    if file.filename:
+        up_file = open("task_files/" + file.filename, "wb+")
+        up_file.write(file.file.read())
+        file.file.close()
+        up_file.close()
+
+    my_database.add_material(id, type, "task_files/" + file.filename)
+
+    return RedirectResponse(f"/task/{id}", status_code=303)
+
+
+@app.get("/add_task/{course}/{level}")
+async def add_task(request: Request, course: str, level: str, name: str):
+    global my_database
+
+    if not check_auth():
+        res = await auth()
+        if not res:
+            return Response(status_code=401)
+
+    my_database.add_task(course, level, name)
+
+    return RedirectResponse(f"/theory_tasks_edit")
+
+
+app.mount("/css", StaticFiles(directory="../templates/css"), "css")
+app.mount("/task_files", StaticFiles(directory="task_files"), "task_files")
